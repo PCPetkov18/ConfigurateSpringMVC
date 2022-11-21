7d8449775668878a1cf029f7684e88c7f14f6808
PCPetkov18 <PCPetkov18@codingburgas.bg>
2022-11-21 11:38:04 +0200

    xml files

---
+++ src/test/java/com/example/configuratespringmvc/ConfigurateSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configuratespringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurateSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
